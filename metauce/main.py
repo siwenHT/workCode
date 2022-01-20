7abe4834c3abff51e79f4dcb690362114238bca1
494631748@qq.com <494631748@qq.com>
2022-01-20 08:43:02 +0800

    一键收矿代码

--- metauce/main.py
+++ metauce/main.py
@@ -1,6 +1,18 @@
 # -*- coding: utf-8 -*-
-
+from thelog import Log
+from selenium import web
 #一键收矿
+
+def do():
+    pass
+#打开网址
+#链接钱包
+#点击菜单第一个
+#点击play
+#点击挖矿
+#点击所有收获
+#等待下一次点击挖矿
+
 
 def main():
     pass
