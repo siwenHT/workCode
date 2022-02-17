135ea491ff2d1a2205b2358e48dc86df14b7a540
494631748@qq.com <494631748@qq.com>
2022-02-17 09:07:48 +0800

    1

--- helper/UI/MainView.py
+++ helper/UI/MainView.py
@@ -10,18 +10,26 @@
 
 # here put the import lib
 import JobManager
+import tkinter as tk
 
 
 class MainView():
 
     def __init__(self, jobManager: JobManager) -> None:
         self._jobManager: JobManager = jobManager
-        pass
+
+        self.InitUI()
 
     def InitUI(self):
-        pass
+        window = tk.Tk()
+        window.title('helper')
+        window.geometry('600x800')
+        window.mainloop()
+
+        self._window = window
 
     def baseView(self):
+        tk.Frame(self._window,)
         pass
 
     def botView(self):
